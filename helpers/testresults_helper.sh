0f5a679718937f3f3a58f9c00c9788df68981357
DESKTOP-2FJQOQP\jobrm <j.obrman@tum.de>
2022-11-06 22:34:16 +0100

    fix exporting folder

--- helpers/testresults_helper.sh
+++ helpers/testresults_helper.sh
@@ -215,7 +215,8 @@ exportExperimentResults() {
         # a pull is not really required, but for small sizes it doesn't hurt
         git pull
         # copy from local folder to git repo folder
-        cp -r ../"$EXPORTPATH" results/
+        [ ! -d results/"${EXPORTPATH::-12}" ] && mkdir results/"${EXPORTPATH::-12}"
+        cp -r ../"$EXPORTPATH" results/"${EXPORTPATH::-12}"
         git add . 
         git commit -a -m "script upload"
         git push 
