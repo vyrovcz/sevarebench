8d823543eae7863199134995ee059f0a388db0aa
DESKTOP-2FJQOQP\jobrm <j.obrman@tum.de>
2022-12-29 17:30:35 +0100

    test custom repo checkout

--- host_scripts/host_setup.sh
+++ host_scripts/host_setup.sh
@@ -46,7 +46,7 @@ cp "$REPO2_DIR"/helpers/htoprc ~/.config/htop/
 
 cd "$REPO_DIR"
 
-# use a stable state of the MP-SPDZ repo
-# git checkout "$REPO_COMMIT"
+# use a custom state of the MP-SPDZ repo
+git checkout "$REPO_COMMIT"
 
 echo "global setup successful"
